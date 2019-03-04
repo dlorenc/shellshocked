41be2ccd1858f29cd5a7ef4c65c4c913e4b93f6d
Dan Lorenc <lorenc.d@gmail.com>
2019-03-04 13:14:36 -0800

    fixing up demo

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@
 set -ex
 version=v1
 project="$(gcloud config get-value project)"
-image="gcr.io/${project}/demo:${version}"
+image="gcr.io/${project}/app:${version}"
 docker build . -t "${image}"
 docker push "${image}"
