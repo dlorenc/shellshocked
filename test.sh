6269f45f4308edb0cddec004fb33b952fa306851
Dan Lorenc <lorenc.d@gmail.com>
2019-04-06 17:14:51 -0500

    Fix the vuln.

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 set -ex
 
 trap "{ exit=$?; docker kill test ; docker rm test; exit $exit; }" EXIT
-
+cd /workspace
 docker build . -t test
-docker run -d -p 8080:80 --name=test test
+docker run --network=cloudbuild -d -p 8080:80 --name=test test
+
+sleep 2
 curl localhost:8080
